962839784a8550853894000e9b52c33eced45926
TomDotScott <tom.scott45456@gmail.com>
2020-12-02 20:19:42 +0000

    Vector class
    A simple vector2i class was created to store coordinate positions.

---
+++ PrimsMaze/Vector2i.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <iostream>
+
+class Vector2
+{
+public:
+	Vector2(int x = 0, int y = 0);
+	Vector2 operator+(const Vector2& rhs) const;
+	Vector2 operator*(int rhs) const;
+	Vector2 operator-(const Vector2& rhs) const;
+	bool operator==(Vector2& rhs) const;
+	bool operator!=(Vector2& rhs) const;
+
+	friend std::ostream& operator<<(std::ostream& os, const Vector2& dt);
+
+	int x;
+	int y;
+};
